5507a0b2cec201580e2295208b0d68cd09fa8258
Xeno <17703938+xenoplayed@users.noreply.github.com>
2022-01-01 17:24:16 +0000

    2.17 making code asynchronous

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,14 +19,14 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<AppUser>> GetUsers() {
-            return _context.Users.ToList();
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() {
+            return await _context.Users.ToListAsync();
             
         }
 
         [HttpGet("{id}")]
-        public ActionResult<AppUser> GetUser(int id) {
-            return _context.Users.Find(id);
+        public async Task<ActionResult<AppUser>> GetUser(int id) {
+            return await _context.Users.FindAsync(id);
         }
     }
 }
